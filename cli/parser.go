605211cb616fb02c8f4abf14504e93328b52dc30
Thiago Leao <thiagogorgulho@outlook.com>
2022-04-23 10:33:13 -0300

    fix typo

--- cli/parser.go
+++ cli/parser.go
@@ -9,7 +9,7 @@ import (
 func ParseCli() *path_walker.ExtChangeJob {
 	pathPtr := flag.String("path", "", "the system path for the desired application")
 	extTypePtr := flag.String("new_extension_type", ".txt", "the new extension type for the files")
-	whitelistPtr := flag.String("whitelist", "()(.changeme)", "the file extensions where the new type will be applied, should be encapsulated in ()")
+	whitelistPtr := flag.String("whitelist", "()(.changName)", "the file extensions where the new type will be applied, should be encapsulated in ()")
 
 	flag.Parse()
 
